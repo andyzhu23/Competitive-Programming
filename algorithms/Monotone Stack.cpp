a22d2b06e5054d863304231c362a02786fa17066
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-03-10 18:37:35 -0800

    Create Monotone Stack.cpp

---
+++ algorithms/Monotone Stack.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e4 + 10;
+
+int n, a[N], ans[N];
+stack<int> stk;
+
+int main(){
+  cin>>n;
+  for(int i =1;i<=n;i++){
+    cin>>a[i];
+    
+    while(!stk.empty() && a[stk.top()] < a[i]){
+      ans[stk.top()] = i;
+      stk.pop();
+    }
+    stk.push(i);
+  }
+  while(!stk.empty()){
+		ans[stk.top()] = n + 1;
+    stk.pop();
+  }
+  for(int i = 1;i<=n;i++) cout<<ans[i] <<" ";
+}
