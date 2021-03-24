fd0f3dd0800b64244f1d371f12756339a4f88474
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-03-24 09:23:15 -0700

    Update CCC20S3.cpp

--- CCC/2020/CCC20S3.cpp
+++ CCC/2020/CCC20S3.cpp
@@ -1,52 +1,67 @@
+/*
+ * Compute permutations by using a frequency array. Use rolling hash to compute the permutations. 
+ * I computed the rolling hash twice using two different bases and mod values.
+ */
+
 #include <iostream>
 #include <unordered_set>
+#define endl "\n"
 using namespace std;
+typedef long long ll;
+const int P = 257;
+const int MOD = 1e9 + 7;
+const int PII = 131;
+const int MODII = 100000007;
+string n, h;
+int needle[29], hay[29], len1, len2;
+unordered_set<ll> vis, vis2;
 
-const int N = 2e5 + 10;
-
-int key[35];
-int hay[N][34];
-int len1,len2;
-string n,h;
-unordered_set<size_t> vis;
-string sub;
-
-bool isPerm(int i){
-    int cur;
-    int j = i + len1 - 1;
-    for(int k = 1;k<=26;k++){
-        cur = hay[j][k] - hay[i-1][k];
-        if(cur != key[k]) return false;
+bool isPerm(){
+    for(int i = 1;i<=27;i++){
+        if(needle[i] != hay[i]) return false;
     }
     return true;
 }
 
-int main() {
-    cin>>n>>h;
-    int ans = 0;
+int main(){
+    cin>>n >>h;
     len1 = n.size();
     len2 = h.size();
     for(int i = 1;i<=len1;i++){
-        key[n[i-1] - 'a' + 1]++;
+        needle[n[i - 1] - 'a' + 1]++;
+
     }
-    for(int i = 1;i<=len2;i++){
-        for(int j = 1;j<=26;j++){
-            hay[i][j] = hay[i-1][j];
-        }
-        hay[i][h[i-1] - 'a' + 1]++;
+    if(len2 < len1) {
+        cout<<0<<endl;
+        return 0;
     }
 
-    for(int i = 1;i<=len2 - len1 + 1;i++){
-        if(isPerm(i)){
-            sub = h.substr(i - 1, len1);
-            hash<string> hasher;
-            size_t hash = hasher(sub);
-            if(!vis.count(hash)){
-                ans++;
-                vis.insert(hash);
-            }
-        }
+    ll power = 1;
+    ll power2 = 1;
+    for (int i = 1; i < len1; i++) {
+        power = (power * P) % MOD;
+        power2 = (power2 * PII) % MODII;
     }
-    cout<<ans;
+    int ans = 0;
+    ll ret = 0;
+    ll ret2 = 0;
+    for(int i = 1; i <=len2;i++){
+        hay[h[i - 1] - 'a' + 1] ++;
+        // add the last character
+        ret = (ret * P + h[i - 1]) % MOD;
+        ret2 = (ret2 * PII + h[i - 1]) % MODII;
+
+        if(i < len1) continue;
+        if (isPerm() && !(vis.count(ret) && vis2.count(ret2))){
+            ans ++;
+            vis.insert(ret);
+            vis2.insert(ret2);
+        }
+        hay[h[i - len1] - 'a' + 1] --;
+        // delete the first character
+        ret = (ret - (power * h[i - len1] % MOD) + MOD) % MOD;
+        ret2 = (ret2 - (power2 * h[i - len1] % MODII) + MODII) % MODII;
+    }
+    cout<<ans<<endl;
     return 0;
 }
