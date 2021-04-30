ed4e1c362057471336d81263e6b9af492289cc5a
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-04-29 21:58:36 -0700

    Update and rename template.cpp to current_template.cpp

--- current_template.cpp
+++ current_template.cpp
@@ -1,8 +1,18 @@
 /*
  * Author: Andy Zhu
+ * 
+ * just a noob, what is coding?
+ *
+ * “Do we run the trails, or the trails run us? Get revenge on the trails that ran u like that” 
+ * I pray the cows
+ * My Dream School:
+ * South Harmon Institute of Technology
+ * The meaning of life is:
+ * #define int long long
  */
 
 #include <iostream>
+#include <fstream>
 #include <cmath>
 #include <vector>
 #include <unordered_set>
@@ -19,11 +29,20 @@
 #include <iterator>
 #include <deque>
 #define endl "\n"
-#define lc (st << 1)
-#define rc (st << 1 | 1)
+#define l_b lower_bound
+#define u_b upper_bound
+#define ins insert
+#define fir first
+#define sec second
+#define lc (rt << 1)
+#define rc (rt << 1 | 1)
 #define pb push_back
+#define pf push_front
 #define mp make_pair
+#define random(a, b) rand() % (b - a + 1) + a
+#define log(a, b) log(a) / log(b)
 using ll = long long;
+using lb = long double;
 using namespace std;
 typedef pair<int, int> PI;
 typedef pair<ll, ll> PLL;
@@ -50,6 +69,11 @@ typedef unordered_map<string, string> UMS;
 const int INF = 1e9;
 const int MOD = 1e9 + 7;
 
+inline void copy(int a[], int b[], int sz){
+    for(int i = 0;i<=sz;i++)
+        a[i] = b[i];
+}
+
 inline int gcd(int a, int b){
     if(a == 0) return b;
     if(b == 0) return a;
@@ -64,11 +88,18 @@ inline int getMid(int l, int r){
     return (l + r) >> 1;
 }
 
-inline int fast_pow(ll a, ll b){
+inline ll fast_pow(ll a, ll b){
     if(b == 1) return a;
     ll tmp = fast_pow(a, b / 2);
-    if(a % 2 == 1) return tmp * tmp * a;
+    if(b % 2 == 1) return tmp * tmp * a;
     else return tmp * tmp;
+}
+
+inline bool is_prime(ll x){
+    for(ll i = 2; i * i <= x;i++){
+        if(x % i == 0) return false;
+    }
+    return true;
 }
 
 int main(){
