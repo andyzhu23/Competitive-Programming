78eabab6a5667a59f81ee78505273dbb668c14ea
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-04-29 21:46:03 -0700

    Create 16S5.cpp

---
+++ CCC/2016/16S5.cpp
@@ -0,0 +1,134 @@
+/*
+ * Author: Andy Zhu
+ *
+ * just a noob, what is coding?
+ *
+ * “Do we run the trails, or the trails run us? Get revenge on the trails that ran u like that”
+ * I pray the cows
+ * My Dream School:
+ * South Harmon Institute of Technology
+ * The meaning of life is:
+ * #define int long long
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <iomanip>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <memory>
+#include <string>
+#include <algorithm>
+#include <bitset>
+#include <iterator>
+#include <deque>
+#define endl "\n"
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
+#define fir first
+#define sec second
+#define lc (rt << 1)
+#define rc (rt << 1 | 1)
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define random(a, b) rand() % (b - a + 1) + a
+#define log(a, b) log(a) / log(b)
+using ll = long long;
+using lb = long double;
+using namespace std;
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef pair<string, string> PS;
+typedef vector<int> VI;
+typedef vector<PI> VPI;
+typedef vector<ll> VLL;
+typedef vector<PLL> VPLL;
+typedef queue<int> QI;
+typedef stack<int> STKI;
+typedef deque<int> DQI;
+typedef set<int> SI;
+typedef set<ll> SLL;
+typedef set<string> SS;
+typedef unordered_set<int> USI;
+typedef unordered_set<ll> USLL;
+typedef unordered_set<string> USS;
+typedef map<int, int> MI;
+typedef map<ll, ll> MLL;
+typedef map<string, string> MS;
+typedef unordered_map<int, int> UMI;
+typedef unordered_map<ll, ll> UMLL;
+typedef unordered_map<string, string> UMS;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+
+inline void copy(int a[], int b[], int sz){
+    for(int i = 0;i<=sz;i++)
+        a[i] = b[i];
+}
+
+inline int gcd(int a, int b){
+    if(a == 0) return b;
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+inline int lcm(int a, int b){
+    return a * b / gcd(a, b);
+}
+
+inline int getMid(int l, int r){
+    return (l + r) >> 1;
+}
+
+inline ll fast_pow(ll a, ll b){
+    if(b == 1) return a;
+    ll tmp = fast_pow(a, b / 2);
+    if(b % 2 == 1) return tmp * tmp * a;
+    else return tmp * tmp;
+}
+
+inline bool is_prime(ll x){
+    for(ll i = 2; i * i <= x;i++){
+        if(x % i == 0) return false;
+    }
+    return true;
+}
+
+const int N = 1e5 + 10;
+int n;
+int a[N], b[N];
+ll t;
+
+int main(){
+    scanf("%d %lld", &n, &t);
+
+    for(int i = 1;i<=n;i++) scanf("%1d", &a[i]);
+    ll l, r;
+    ll cur = 0;
+    for(ll k = fast_pow(2, 60); cur < t; k = (k >> 1)){
+        if(cur + k > t) continue;
+        for(ll j = 1;j<=n;j++){
+            if(j - k > 0) l = j - k;
+            else l = n - abs(j - k) % n;
+            if(j + k <= n) r = j + k;
+            else r = (j + k) % n;
+            if(l == 0) l = n;
+            if(r == 0) r = n;
+            b[j] = a[l] ^ a[r];
+        }
+        cur += k;
+        copy(a, b, n);
+    }
+    for(int i = 1;i<=n;i++){
+        printf("%d", a[i]);
+    }
+    return 0;
+}
