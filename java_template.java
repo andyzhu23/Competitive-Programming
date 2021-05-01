7f9d926bb7053c035238b3d650462414f23efb53
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-04-30 17:55:59 -0700

    Update java_template.java

--- java_template.java
+++ java_template.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Main(){
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        System.out.println("hello world");
+        
         br.close();
     }
 }
