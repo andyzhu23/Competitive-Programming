9709be855ebb4ce20eb5b55c7dc77d9d2f46212d
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-04-30 17:55:44 -0700

    Create java_template.java

---
+++ java_template.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main(){
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        System.out.println("hello world");
+        br.close();
+    }
+}
