ac3fda6e665348b4157ea83124361d5ba7ab6617
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-05-29 20:27:29 -0700

    Update TSP_问题.cpp

--- other solutions/TSP_问题.cpp
+++ other solutions/TSP_问题.cpp
@@ -3,17 +3,21 @@
 #include <memory.h>
 #define endl "\n"
 using namespace std;
-const int N = 20;
+const int N = 18;
 const int INF = 0x3f3f3f3f;
-int e[N][N], n, dp[N][1 << N];
+int e[N][N], n, dp[N][1 << N], final;
 
-inline void dfs(int u, int fa, int mask, int last){
-    if(fa == -1) dp[u][mask] = 0;
-    else dp[u][mask] = min(dp[u][mask], dp[fa][last] + e[fa][u]);
-    for(int v = 0;v<=n;v++){
-        if(mask & (1 << v)) continue;
-        dfs(v, u, mask | (1 << v), mask);
+inline int dfs(int u, int mask){
+    if(dp[u][mask] >= INF){
+        if(mask == final) dp[u][mask] = e[u][0];
+        else {
+            for(int v = 0;v<=n;v++){
+                if(mask & 1 << v) continue;
+                dp[u][mask] = min(dp[u][mask], dfs(v, mask | 1 << v) + e[u][v]);
+            }
+        }
     }
+    return dp[u][mask];
 }
 
 int main(){
@@ -21,16 +25,12 @@ int main(){
     cin.tie(0);
     cout.tie(0);
     cin>>n;
+    final = (1 << (n + 1)) - 1;
     memset(dp, 63, sizeof(dp));
     for(int i = 0;i<=n;i++)
         for(int j = 0;j<=n;j++)
             cin>>e[i][j];
-    dfs(0, -1, 1, 0);
-    int final = (1 << (n + 1)) - 1;
-    int ans = INF;
-    for(int i = 0;i<=n;i++) {
-        ans = min(ans, dp[i][final] + e[i][0]);
-    }
-    cout<<ans<<endl;
+    dfs(0, 1);
+    cout<<dp[0][1]<<endl;
     return 0;
 }
