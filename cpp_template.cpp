370af2d4dea0655f579aefbcb080665c4d149143
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-06-26 08:13:27 -0700

    Update cpp_template.cpp

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 // define
 #define endl "\n"
-#define l_b lower_bound
-#define u_b upper_bound
+#define lb lower_bound
+#define ub upper_bound
 #define ins insert
 #define fir first
 #define sec second
 #define lc (rt << 1)
 #define rc (rt << 1 | 1)
-#define ep emplace_back
+#define eb emplace_back
 #define ef emplace_front
 #define pb push_back
 #define pf push_front
@@ -29,7 +29,7 @@ using namespace std;
 // Data Structure Shorten
 using ll = long long;
 using ull = unsigned long long;
-using lb = long double;
+using ld = long double;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 using qi = queue<int>;
@@ -56,10 +56,8 @@ template <typename T> inline void cmax(T& u, T v) { u = max(u, v); }
 template <typename T> inline void cmin(T& u, T v) { u = min(u, v); }
 
 template <typename T> inline T lowbit(T x){return x & (- x);}
-template <typename T> inline T gcd(T a, T b){if(a == 0) return b; if(b == 0) return a; return gcd(b, a % b);}
-template <typename T> inline T lcm(T a, T b){return a / gcd(a, b) * b;}
-template <typename T> inline T getMid(T l, T r){return (l + r) >> 1;}
-template <typename T1, typename T2> inline T1 fast_pow(T1 a, T2 b){if(b == 1) return a; T1 tmp = fast_pow(a, b / 2); if(b % 2 == 1) return tmp * tmp * a; else return tmp * tmp;}
+template <typename T> inline T __lcm(T a, T b){return a / __gcd(a, b) * b;}
+template <typename T1, typename T2> inline T1 fast_pow(T1 a, T2 b){if(b == 1) return a; T1 tmp = fast_pow(a, b >> 1); if(b & 1) return tmp * tmp * a; else return tmp * tmp;}
 template <typename T> inline bool is_prime(T x){if(x == 1) return false; for(T i = 2; i * i <= x;i++) if(x % i == 0) return false; return true;}
 
 template <typename T> inline void debugArray(T *arr,int sz){cout<<"[";for(int i=0;i<sz;i++){cout<<arr[i]<<", "; } cout<<"]\n";}
@@ -91,29 +89,30 @@ inline void solve(){
 //---------------------  end of program  ---------------------
 
 
-bool doCase = 0;
-bool config = 0;
-bool kickstart = 0;
+#define doCase 0
+#define config 0
+#define kickstart 0
 
 int main(){
-    if(config){
+    #if config
         // configuration here
         freopen(".in", "r", stdin);
         freopen(".out", "w", stdout);
-    }
+    #endif
     srand(time(0));
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     init1();
-    if(doCase) {
+    #if doCase
         int t; cin>>t;
         for(int i = 1;i<=t;i++) {
             if(kickstart) cout<<"Case #"<< i<<": ";
             init2();
             solve();
         }
-    } else solve();
+    #else solve();
+    #endif
     string jack = "Jack is always within you";
     return 0;
 }
