57443dea9efbbd9a02bf8e39bb1f3be1cec5ae1d
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-07-04 15:46:00 -0700

    Update cpp_template.cpp

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -1,5 +1,7 @@
 /*
  * Author: Andy Zhu
+ * @date    2021-07-04 15:23:19
+ * @version 1.0.0
  */
 
 //include
@@ -25,6 +27,7 @@ using namespace std;
 #define ALL(v) v.begin(), v.end()
 #define clr(x) memset(x, 0, sizeof(x));
 #define setINF(x) memset(x, 63, sizeof(x));
+#define setNINF(x) memset(x, -63, sizeof(x));
 
 // Data Structure Shorten
 using ll = long long;
@@ -35,14 +38,20 @@ using pll = pair<ll, ll>;
 using qi = queue<int>;
 using stki = stack<int>;
 template <typename T> using vec = vector<T>;
-template <typename T> using uset = unordered_set<T>;
-template <typename T1, typename T2> using umap = unordered_map<T1, T2>;
+template <typename T> using us = unordered_set<T>;
+template <typename T> using os = set<T>;
+template <typename T1, typename T2> using um = unordered_map<T1, T2>;
+template <typename T1, typename T2> using om = map<T1, T2>;
 template <typename T> using pq = priority_queue<T>; 
 template <typename T> using pqg = priority_queue<T, vec<T>, greater<T> >;
 using vi = vec<int>;
 using vpii = vec<pii>;
 using vll = vec<ll>;
 using vpll = vec<pll>;
+using vb = vec<bool>;
+
+// loops!
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
 
 // common variables
 const int INF = 0x3f3f3f3f;
@@ -57,7 +66,10 @@ template <typename T> inline void cmin(T& u, T v) { u = min(u, v); }
 
 template <typename T> inline T lowbit(T x){return x & (- x);}
 template <typename T> inline T __lcm(T a, T b){return a / __gcd(a, b) * b;}
-template <typename T1, typename T2> inline T1 fast_pow(T1 a, T2 b){if(b == 1) return a; T1 tmp = fast_pow(a, b >> 1); if(b & 1) return tmp * tmp * a; else return tmp * tmp;}
+template <typename T1, typename T2> inline T1 fast_pow(T1 a, T2 b)
+{if(b == 1) return a; T1 tmp = fast_pow(a, b >> 1); if(b & 1) return tmp * tmp * a; else return tmp * tmp;}
+template <typename T1, typename T2, typename T3> inline T1 fast_pow(T1 a, T2 b, T3 m)
+{if(b == 1) return a % m; T1 tmp = fast_pow(a, b >> 1); if(b & 1) return tmp * tmp % m * a % m; else return tmp * tmp % m;}
 template <typename T> inline bool is_prime(T x){if(x == 1) return false; for(T i = 2; i * i <= x;i++) if(x % i == 0) return false; return true;}
 
 template <typename T> inline void debugArray(T *arr,int sz){cout<<"[";for(int i=0;i<sz;i++){cout<<arr[i]<<", "; } cout<<"]\n";}
@@ -65,14 +77,13 @@ template <typename T> inline void printArray(T *arr,int sz){for(int i=0;i<sz;i++
 
 
 //------------------- start of initialize -------------------
-
+// initialize for all cases
 inline void init1(){
-    // initialize for all cases
 
 }
 
+// initialize for the current subcase
 inline void init2(){
-    // initialize for the current subcase
 
 }
 
@@ -92,18 +103,25 @@ inline void solve(){
 #define doCase 0
 #define config 0
 #define kickstart 0
+#define unsync 1
 
-int main(){
+inline void setIO() {
     #if config
-        // configuration here
-        freopen(".in", "r", stdin);
-        freopen(".out", "w", stdout);
+    // configuration here
+    freopen(".in", "r", stdin);
+    freopen(".out", "w", stdout);
     #endif
-    srand(time(0));
+    #if unsync
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+    #endif
+}
+
+int main(){
+    setIO();
     init1();
+    srand(time(0));
     #if doCase
     int t; cin>>t;
     for(int i = 1;i<=t;i++) {
