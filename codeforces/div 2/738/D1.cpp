33f71845d46d339b2cab7f7a28757625fd1826e5
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-08-15 11:52:49 -0700

    Update and rename D.cpp to D1.cpp

--- codeforces/div 2/738/D1.cpp
+++ codeforces/div 2/738/D1.cpp
@@ -3,11 +3,11 @@
  * @date    2021-08-15 07:33:14
  * @version 1.0.0
  */
-
+ 
 //include
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 // define
 #define endl "\n"
 #define lb lower_bound
@@ -28,7 +28,7 @@ using namespace std;
 #define clr(x) memset(x, 0, sizeof(x))
 #define setINF(x) memset(x, 63, sizeof(x))
 #define setNINF(x) memset(x, -63, sizeof(x))
-
+ 
 // Data Structure Shorten
 using ll = long long;
 using ull = unsigned long long;
@@ -49,21 +49,21 @@ using vpii = vec<pii>;
 using vll = vec<ll>;
 using vpll = vec<pll>;
 using vb = vec<bool>;
-
+ 
 // loops!
 #define FOR(i, a, b) for(int i = (a); i < (b); ++i)
-
+ 
 // common variables
 const int INF = 0x3f3f3f3f;
 const ll LLINF = 0x3f3f3f3f3f3f3f3f;
 const int MOD = 1e9 + 7;
 const int dir[8][2] = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}, {-1, 1}, {1, -1}, {-1, -1}, {1, 1}};
 struct Edge {int u, v, w;};
-
+ 
 // common functions
 template <typename T> inline void cmax(T& u, T v) { u = max(u, v); }
 template <typename T> inline void cmin(T& u, T v) { u = min(u, v); }
-
+ 
 template <typename T> inline T lowbit(T x){return x & (- x);}
 template <typename T> inline T gcd(T a, T b){if(a == 0) return b; if(b == 0) return a; return gcd(b, a % b);}
 template <typename T> inline T lcm(T a, T b){return a / gcd(a, b) * b;}
@@ -72,41 +72,39 @@ template <typename T1, typename T2> inline T1 fast_pow(T1 a, T2 b)
 template <typename T1, typename T2, typename T3> inline T1 fast_pow(T1 a, T2 b, T3 m)
 {if(b == 1) return a % m; T1 tmp = fast_pow(a, b >> 1, m); if(b & 1) return tmp * tmp % m * a % m; else return tmp * tmp % m;}
 template <typename T> inline bool is_prime(T x){if(x == 1) return false; for(T i = 2; i * i <= x;i++) if(x % i == 0) return false; return true;}
-
+ 
 template <typename T> inline void debugArray(T *arr,int sz){cout<<"[";for(int i=0;i<sz;i++){cout<<arr[i]<<", "; } cout<<"]\n";}
 template <typename T> inline void printArray(T *arr,int sz){for(int i=0;i<sz;i++){cout<<arr[i]<<" "; } cout<<"\n";}
-
-
+ 
+ 
 //------------------- start of initialize -------------------
 // initialize for all cases
 inline void init1(){
-
+ 
 }
-
+ 
 // initialize for the current subcase
 inline void init2(){
-
+ 
 }
-
+ 
 //-------------------  end of initialize  -------------------
-
+ 
 //--------------------- start of program ---------------------
-
-const int N = 1e5 + 10;
+ 
+const int N = 1e3 + 10;
 int n, m1, m2, f1[N], f2[N];
-us<int> vis;
-vpii e;
-
+ 
 int Find(int x, int f[N]) {return x == f[x] ? x : f[x] = Find(f[x], f);}
-
+ 
 bool merge(int u, int v, int f[N], bool flag) {
     int fu = Find(u, f);
     int fv = Find(v, f);
     if(fu == fv) return 0;
-    if(flag) f[fv] = fu;
+    if(flag) f[fu] = fv;
     return 1;
 }
-
+ 
 inline void solve(){
     cin>>n>>m1>>m2;
     for(int i = 1;i<=n;i++) {
@@ -116,44 +114,37 @@ inline void solve(){
     while(m1 --) {
         int u, v;
         cin>>u>>v;
-        e.pb(mp(u, v));
         merge(u, v, f1, 1);
     }
-    
     while(m2 --) {
         int u, v;
         cin>>u>>v;
-        e.pb(mp(u, v));
         merge(u, v, f2, 1);
     }
     int ans = 0;
     vpii a;
-    int prev = -1;
-    for(int k = 1;k<=min(998, n - 1);k++) {
-        for(int i = 1;i<=n;i++) {
-            int j = i + k;
-            if(j > n) j %= n;
+    for(int i = 1;i<=n;i++) {
+        for(int j = 1;j<=n;j++) {
             if(merge(i, j, f1, 0) && merge(i, j, f2, 0)) {
                 ans++;
                 a.pb(mp(i, j));
                 merge(i, j, f1, 1), merge(i, j, f2, 1);
-            } 
+            }
         }
     }
-
     cout<<ans<<'\n';
     for(pii i : a) cout<<i.fir<<" "<<i.sec<<'\n';
 }
-
-
+ 
+ 
 //---------------------  end of program  ---------------------
-
-
+ 
+ 
 #define doCase 0
 #define config 0
 #define kickstart 0
 #define unsync 1
-
+ 
 inline void setIO() {
     #if config
     // configuration here
@@ -166,7 +157,7 @@ inline void setIO() {
     cout.tie(0);
     #endif
 }
-
+ 
 int main(){
     setIO();
     init1();
@@ -185,7 +176,7 @@ int main(){
     // 84ecef820ae07338
     return 0;
 }
-
+ 
  
 /* stuff you should look for
     * int overflow, array bounds
