3c4725b915f0e5da20f957aa1abcc461290bff86
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-08-17 09:42:15 -0700

    Update Treap.cpp

--- algorithms/balanced tree/Treap.cpp
+++ algorithms/balanced tree/Treap.cpp
@@ -1,51 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-using pii = pair<int, int>;
 
 const int N = 1e5 + 10;
 
-template <typename T> struct BST{
-    T val[N], lc[N], rc[N], cnt[N], tot, root, size[N], rnd[N];
-
-    T& Find(int& rt, int x) {
-        if(rt == 0) return rt;
-        if(val[rt] == x) return rt;
-        if(val[rt] > x) return Find(lc[rt], x);
-        else return Find(rc[rt], x);
-    }
+template <typename T> struct BST {
+    int lc[N], rc[N], sz[N], rnd[N], cnt[N], root, tot;
+    T val[N];
 
     void push_up(int rt) {
-        size[rt] = size[lc[rt]] + size[rc[rt]] + cnt[rt];
-    }
-
-    void rturn(int& rt) {
-        int tmp = lc[rt];
-        lc[rt] = rc[lc[rt]];
-        rc[tmp] = rt;
-        push_up(rt);
-        rt = tmp;
-        push_up(rt);
+        sz[rt] = sz[lc[rt]] + sz[rc[rt]] + cnt[rt];
     }
 
     void lturn(int& rt) {
         int tmp = rc[rt];
-        rc[rt] = lc[rc[rt]];
+        rc[rt] = lc[tmp];
         lc[tmp] = rt;
         push_up(rt);
         rt = tmp;
         push_up(rt);
     }
 
-    void insert(int& rt, int x) {
+    void rturn(int& rt) {
+        int tmp = lc[rt];
+        lc[rt] = rc[tmp];
+        rc[tmp] = rt;
+        push_up(rt);
+        rt = tmp;
+        push_up(rt);
+    }
+
+    void insert(int& rt, T x) {
         if(rt == 0) {
             rt = ++tot;
-            size[rt] = 1;
             val[rt] = x;
             cnt[rt] = 1;
+            sz[rt] = 1;
             rnd[rt] = rand();
             return;
-        }
-        if(val[rt] == x) cnt[rt] ++;
+        } else if(val[rt] == x) cnt[rt] ++;
         else if(val[rt] > x) {
             insert(lc[rt], x);
             if(rnd[lc[rt]] > rnd[rt]) rturn(rt);
@@ -57,25 +49,7 @@ template <typename T> struct BST{
         push_up(rt);
     }
 
-    int succans;
-    void succ(int& rt, int x) {
-        if(rt == 0) return;
-        if(val[rt] > x) {
-            succans = val[rt];
-            succ(lc[rt], x);
-        } else succ(rc[rt], x);
-    }
-
-    int preans;
-    void pre(int& rt, int x) {
-        if(rt == 0) return;
-        if(val[rt] < x) {
-            preans = val[rt];
-            pre(rc[rt], x);
-        } else pre(lc[rt], x);
-    }
-
-    void erase(int& rt, int x) {
+    void erase(int& rt, T x) {
         if(rt == 0) return;
         if(val[rt] == x) {
             if(cnt[rt] - 1 == 0) {
@@ -85,57 +59,102 @@ template <typename T> struct BST{
                     else lturn(rt);
                     erase(rt, x);
                 }
-            } else cnt[rt]--;
-        } 
-        else if(val[rt] > x) erase(lc[rt], x);
+            } else cnt[rt] --;
+        } else if(val[rt] > x) erase(lc[rt], x);
         else erase(rc[rt], x);
         push_up(rt);
     }
 
-    int rnk(int& rt, int x) {
-        if(rt == 0) return 1;
-        if(val[rt] == x) return size[lc[rt]] + 1; 
-        if(val[rt] > x) return rnk(lc[rt], x);
-        else return rnk(rc[rt], x) + size[lc[rt]] + cnt[rt];
+    int Find(int& rt, T x) {
+        if(val[rt] == x) return rt;
+        else if(val[rt] > x) return Find(lc[rt], x);
+        else return Find(rc[rt], x);
     }
 
-    int id(int& rt, int x) {
-        if(rt == 0) return 0;
-        if(size[lc[rt]] >= x) return id(lc[rt], x);
-        else if(size[lc[rt]] + cnt[rt] >= x) return val[rt];
-        else return id(rc[rt], x - size[lc[rt]] - cnt[rt]);
+    int rnk(int& rt, T x) {
+        if(rt == 0) return -1;
+        if(val[rt] == x) return sz[lc[rt]] + 1;
+        else if(val[rt] > x) return rnk(lc[rt], x);
+        int tmp = rnk(rc[rt], x);
+        return tmp == -1 ? -1 : tmp + sz[lc[rt]] + cnt[rt];
     }
-    void print(int rt) {
+
+    T id(int& rt, int x) {
+        if(rt == 0) return -1;
+        if(sz[lc[rt]] >= x) return id(lc[rt], x);
+        else if(sz[lc[rt]] + cnt[rt] >= x) return val[rt];
+        else return id(rc[rt], x - sz[lc[rt]] - cnt[rt]);
+    }
+
+    T succans;
+    void succ(int& rt, T x) {
+        if(rt == 0) return;
+        if(val[rt] > x) {
+            succans = val[rt];
+            succ(lc[rt], x);
+        } else succ(rc[rt], x);
+    }
+    T preans;
+    void pre(int& rt, T x) {
+        if(rt == 0) return;
+        if(val[rt] < x) {
+            preans = val[rt];
+            pre(rc[rt], x);
+        } else pre(lc[rt], x);
+    }
+
+    void debug(int rt){
     	if(rt == 0) return;
-    	printf("id: %d, rt:%d, lc:%d, rc:%d, sz:%d, cnt:%d\n", rt, val[rt], lc[rt], rc[rt], size[rt], cnt[rt]);
-    	print(lc[rt]);
-    	print(rc[rt]);
+        cout<<"rt: "<<val[rt]<<" ";
+    	printf("id: %d, lc:%d, rc:%d, sz:%d, cnt:%d, val:\n", rt, lc[rt], rc[rt], sz[rt], cnt[rt]);
+    	debug(lc[rt]);
+    	debug(rc[rt]);
+	}
+
+    void print(int& rt, int x) {
+        if(rt == 0) return;
+        print(lc[rt], x);
+        for(int i = 0;i<cnt[rt];i++)
+            cout<<val[rt]<<' ';
+        print(rc[rt], x);
     }
+
 };
 
 BST<int> bst;
-int n, a[N];
-map<int, int> id;
 
-int main(){
-    srand(time(NULL));
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin>>n;
-    for(int i = 1;i<=n;i++) {
+    while(n--) {
         int opt, x;
         cin>>opt>>x;
-        if(opt == 1) bst.insert(bst.root, x);
-        else if(opt == 2) bst.erase(bst.root, x);
-        else if(opt == 3) cout<<bst.rnk(bst.root, x)<<'\n';
-        else if(opt == 4) cout<<bst.id(bst.root, x)<<'\n';
-        else if(opt == 5) {
-            bst.pre(bst.root, x);
-            cout<<bst.preans<<'\n';
-        }
-        else {
-            bst.succ(bst.root, x);
-            cout<<bst.succans<<'\n';
+        switch (opt) {
+            case 1:
+                bst.insert(bst.root, x);
+                break;
+            case 2:
+                bst.erase(bst.root, x);
+                break;
+            case 3:
+                cout<<bst.rnk(bst.root, x)<<'\n';
+                break;
+            case 4:
+                cout<<bst.id(bst.root, x)<<'\n';
+                break;
+            case 6:
+                bst.succ(bst.root, x);
+                cout<<bst.succans<<'\n';
+                break;
+            case 5:
+                bst.pre(bst.root, x);
+                cout<<bst.preans<<'\n';
+                break;
         }
     }
+
     return 0;
 }
-
