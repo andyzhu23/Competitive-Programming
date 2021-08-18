8483711417d18e2f3f4731b75e94033ae8ac18eb
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-08-17 21:30:33 -0700

    Create 部落战争.cpp

---
+++ other solutions/部落战争.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ok(x, y) (x > 0 && x <= m && y > 0 && y <= n && s[y - 1] == '.')
+#define id(x, y) ((x - 1) * n + y)
+#define pb push_back
+const int N = 60;
+const int NN = N * N;
+int m, n, r, c, tot, match[NN], vis[NN];
+set<int> e[N], lft;
+
+bool Find(int u) {
+    for(auto& v : e[u]) {
+        if(vis[v]) continue;
+        vis[v] = 1;
+        if(match[v] == 0 || Find(match[v])) {
+            match[v] = u;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>m>>n>>r>>c;
+    int dir[4][2] = {{r, c}, {r, -c}, {c, r}, {c, -r}};
+    for(int i = 1;i<=m;i++) {
+        string s; cin>>s;
+        for(int j = 1;j<=n;j++) {
+            tot += s[j - 1] == '.';
+            if(s[j - 1] != '.') continue;
+            for(int k = 0;k<4;k++) {
+                int x = i + dir[k][0];
+                int y = j + dir[k][1];
+                if(ok(x, y)) {
+                    e[id(i, j)].insert(id(x, y));
+                    lft.insert(id(i, j));
+                }
+            }
+        }
+    }
+    int ans = 0;
+    for(auto& i : lft) {
+        memset(vis, 0, sizeof(vis));
+        if(Find(i)) ans ++;
+    }
+    cout<<tot - ans<<'\n';
+    return 0;
+}
