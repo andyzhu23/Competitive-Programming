e1ad2af7f3ff79098e2acec9e41d16b7b471ccc2
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-08-18 12:13:19 -0700

    Update 部落战争.cpp

--- other solutions/部落战争.cpp
+++ other solutions/部落战争.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ok(x, y) (x > 0 && x <= m && y > 0 && y <= n && s[y - 1] == '.')
+#define ok(x, y) (x > 0 && x <= m && y > 0 && y <= n && a[x - 1][y - 1] == '.')
 #define id(x, y) ((x - 1) * n + y)
 #define pb push_back
 const int N = 60;
 const int NN = N * N;
 int m, n, r, c, tot, match[NN], vis[NN];
-set<int> e[N], lft;
+set<int> e[NN], lft;
+string a[N];
 
 bool Find(int u) {
     for(auto& v : e[u]) {
@@ -27,10 +28,12 @@ int main() {
     cin>>m>>n>>r>>c;
     int dir[4][2] = {{r, c}, {r, -c}, {c, r}, {c, -r}};
     for(int i = 1;i<=m;i++) {
-        string s; cin>>s;
+        cin>>a[i - 1];
+    }
+    for(int i = 1;i<=m;i++) {
         for(int j = 1;j<=n;j++) {
-            tot += s[j - 1] == '.';
-            if(s[j - 1] != '.') continue;
+            tot += a[i - 1][j - 1] == '.';
+            if(a[i - 1][j - 1] != '.') continue;
             for(int k = 0;k<4;k++) {
                 int x = i + dir[k][0];
                 int y = j + dir[k][1];
