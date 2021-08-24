85f0b79fd38383efee1c39964f3a2e1a5aa05597
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-08-23 21:36:34 -0700

    Update cpp_template.cpp

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -9,25 +9,36 @@
 using namespace std;
 
 // define
+#define OJ ONLINE_JUDGE
 #define endl "\n"
-#define lb lower_bound
-#define ub upper_bound
-#define ins insert
+
+// pairs
 #define fir first
 #define sec second
+#define mp make_pair
+
+// segment tree
 #define lc (rt << 1)
 #define rc (rt << 1 | 1)
+
+// STL Data Structures
 #define eb emplace_back
 #define ef emplace_front
 #define pb push_back
 #define pf push_front
-#define mp make_pair
-#define random(a, b) rand() % (b - a + 1) + a
+#define all(v) v.begin(), v.end()
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+
+// math
 #define log(a, b) log(a) / log(b)
-#define ALL(v) v.begin(), v.end()
-#define clr(x) memset(x, 0, sizeof(x));
-#define setINF(x) memset(x, 63, sizeof(x));
-#define setNINF(x) memset(x, -63, sizeof(x));
+
+// memset
+#define mset(x, y) memset(x, y, sizeof(x))
+
+// random
+#define random(a, b) rand() % (b - a + 1) + a
 
 // Data Structure Shorten
 using ll = long long;
@@ -40,6 +51,7 @@ using stki = stack<int>;
 template <typename T> using vec = vector<T>;
 template <typename T> using us = unordered_set<T>;
 template <typename T> using os = set<T>;
+template <typename T> using ms = multiset<T>;
 template <typename T1, typename T2> using um = unordered_map<T1, T2>;
 template <typename T1, typename T2> using om = map<T1, T2>;
 template <typename T> using pq = priority_queue<T>; 
@@ -61,20 +73,36 @@ const int dir[8][2] = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}, {-1, 1}, {1, -1}, {-1, 
 struct Edge {int u, v, w;};
 
 // common functions
-template <typename T> inline void cmax(T& u, T v) { u = max(u, v); }
-template <typename T> inline void cmin(T& u, T v) { u = min(u, v); }
 
-template <typename T> inline T lowbit(T x){return x & (- x);}
-template <typename T> inline T __lcm(T a, T b){return a / __gcd(a, b) * b;}
-template <typename T1, typename T2> inline T1 fast_pow(T1 a, T2 b)
-{if(b == 1) return a; T1 tmp = fast_pow(a, b >> 1); if(b & 1) return tmp * tmp * a; else return tmp * tmp;}
-template <typename T1, typename T2, typename T3> inline T1 fast_pow(T1 a, T2 b, T3 m)
-{if(b == 1) return a % m; T1 tmp = fast_pow(a, b >> 1, m); if(b & 1) return tmp * tmp % m * a % m; else return tmp * tmp % m;}
-template <typename T> inline bool is_prime(T x){if(x == 1) return false; for(T i = 2; i * i <= x;i++) if(x % i == 0) return false; return true;}
+namespace comfun {
+    template <typename T> inline void cmax(T& u, T v) { u = max(u, v); }
+    template <typename T> inline void cmin(T& u, T v) { u = min(u, v); }
+    template <typename T> inline T lowbit(T x){return x & (- x);}
+    template <typename T> inline T gcd(T a, T b){if(a == 0) return b; if(b == 0) return a; return gcd(b, a % b);}
+    template <typename T> inline T lcm(T a, T b){return a / gcd(a, b) * b;}
+    template <typename T1, typename T2> inline T1 fast_pow(T1 a, T2 b)
+    {if(b == 1) return a; if(b == 0) return 1; T1 tmp = fast_pow(a, b >> 1); if(b & 1) return tmp * tmp * a; else return tmp * tmp;}
+    template <typename T1, typename T2, typename T3> inline T1 fast_pow(T1 a, T2 b, T3 m)
+    {if(b == 1) return a % m; if(b == 0) return 1; T1 tmp = fast_pow(a, b >> 1, m); if(b & 1) return tmp * tmp % m * a % m; else return tmp * tmp % m;}
+    template <typename T> inline bool is_prime(T x){if(x == 1) return false; for(T i = 2; i * i <= x;i++) if(x % i == 0) return false; return true;}
+}
+namespace debug {
+    template <typename T> inline void debugArray(T *arr,int sz){cout<<"[";for(int i=0;i<sz;i++){cout<<arr[i]<<", "; } cout<<"]\n";}
+    template <typename T> inline void printArray(T *arr,int sz){for(int i=0;i<sz;i++){cout<<arr[i]<<" "; } cout<<"\n";}
+}
+namespace fast_io {
+    void read(int &number) {
+        bool negative = false; int c; number = 0; c = getchar();
+        if (c == '-') {negative = true;c = getchar();}
+        for (; (c > 47 && c < 58); c = getchar())number = number *10 + c - 48;
+        if (negative) number *= -1;
+    }
+}
 
-template <typename T> inline void debugArray(T *arr,int sz){cout<<"[";for(int i=0;i<sz;i++){cout<<arr[i]<<", "; } cout<<"]\n";}
-template <typename T> inline void printArray(T *arr,int sz){for(int i=0;i<sz;i++){cout<<arr[i]<<" "; } cout<<"\n";}
-
+// using namespaces
+using namespace comfun;
+using namespace debug;
+using namespace fast_io;
 
 //------------------- start of initialize -------------------
 // initialize for all cases
@@ -123,7 +151,7 @@ int main(){
     init1();
     srand(time(0));
     #if doCase
-    int t; cin>>t;
+    int t; read(t);
     for(int i = 1;i<=t;i++) {
         if(kickstart) cout<<"Case #"<< i<<": ";
         init2();
@@ -135,3 +163,12 @@ int main(){
     string jack = "Jack is always within you";
     return 0;
 }
+
+ 
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
