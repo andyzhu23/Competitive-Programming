aa59d95ce252cbd720082214721d5dbfe441868d
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-08-31 20:53:36 -0700

    Update java_template.java

--- java_template.java
+++ java_template.java
@@ -1,11 +1,40 @@
-
 import java.util.*;
 import java.io.*;
 
-public class Main(){
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main{
+    static IO io = new IO();
     public static void main(String[] args) throws IOException {
         
-        br.close();
+    }
+}
+
+class IO {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    String next() throws IOException {
+        while(st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    String readString() throws IOException {
+        return next();
+    }
+    char readChar() throws IOException {
+        return next().charAt(0);
+    }
+    short readShort () throws IOException {
+        return Short.parseShort(next());
+    }
+    double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    String readLine () throws IOException {
+        return br.readLine().trim();
     }
 }
