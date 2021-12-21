da8f39ba21eabd6d9a775b9816b2d557b9880a09
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-12-21 13:27:50 -0800

    Create P1.cpp

---
+++ DMOPC/21/Dec/P1.cpp
@@ -0,0 +1,241 @@
+/*
+ * Author: Andy Zhu
+ * @date    2021-12-19 13:31:41
+ * @version 1.0.0
+ */
+
+// optimize
+#pragma GCC optimize(2)
+
+//include
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+// define
+#define OJ ONLINE_JUDGE
+#define endl "\n"
+#define rint registered int
+
+// pairs
+#define fir first
+#define sec second
+#define mp make_pair
+
+// segment tree
+#define lc (rt << 1)
+#define rc (rt << 1 | 1)
+
+// STL Data Structures
+#define eb emplace_back
+#define ef emplace_front
+#define pb push_back
+#define pf push_front
+#define all(v) v.begin(), v.end()
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+
+// math
+#define log(a, b) log(a) / log(b)
+
+// memset
+#define mset(x, y) memset(x, y, sizeof(x))
+
+// random
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define random(a, b) rng() % (b - a + 1) + a
+
+// Data Structure Shorten
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using db = double;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using qi = queue<int>;
+using stki = stack<int>;
+template <typename T> using vec = vector<T>;
+template <typename T> using us = unordered_set<T>;
+template <typename T> using os = set<T>;
+template <typename T> using ms = multiset<T>;
+template <typename T1, typename T2> using um = unordered_map<T1, T2>;
+template <typename T1, typename T2> using om = map<T1, T2>;
+template <typename T> using pq = priority_queue<T>;
+template <typename T> using pqg = priority_queue<T, vector<T>, greater<T> >;
+using vi = vector<int>;
+using vpii = vector<pair<int, int> >;
+using vll = vector<long long>;
+using vpll = vector<pair<long long, long long> >;
+using vb = vector<bool>;
+
+// loops!
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+
+// common variables
+const int INF = 0x3f3f3f3f;
+const long long LLINF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9 + 7;
+const int dir[8][2] = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}, {-1, 1}, {1, -1}, {-1, -1}, {1, 1}};
+struct Edge {int u, v, w;};
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator() (const std::pair<T1, T2> &pair) const {
+        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    }
+};
+
+// common functions
+
+namespace comfun {
+    template <typename T> inline void cmax(T& u, T v) { u = max(u, v); }
+    template <typename T> inline void cmin(T& u, T v) { u = min(u, v); }
+    template <typename T> inline T lowbit(T x){return x & (- x);}
+    template <typename T> inline T gcd(T a, T b){if(a == 0) return b; if(b == 0) return a; return gcd(b, a % b);}
+    template <typename T> inline T lcm(T a, T b){return a / gcd(a, b) * b;}
+    template <typename T1, typename T2> inline T1 fast_pow(T1 a, T2 b)
+    {if(b == 1) return a; if(b == 0) return 1; T1 tmp = fast_pow(a, b >> 1); if(b & 1) return tmp * tmp * a; else return tmp * tmp;}
+    template <typename T1, typename T2, typename T3> inline T1 fast_pow(T1 a, T2 b, T3 m)
+    {if(b == 1) return a % m; if(b == 0) return 1; T1 tmp = fast_pow(a, b >> 1, m); if(b & 1) return tmp * tmp % m * a % m; else return tmp * tmp % m;}
+    template <typename T> inline bool is_prime(T x){if(x == 1) return false; for(T i = 2; i * i <= x;i++) if(x % i == 0) return false; return true;}
+}
+namespace debug {
+    template <typename T> inline void debugArray(T *arr,int sz){cout<<"[";for(int i=0;i<sz;i++){cout<<arr[i]<<", "; } cout<<"]\n";}
+    template <typename T> inline void printArray(T *arr,int sz){for(int i=0;i<sz;i++){cout<<arr[i]<<" "; } cout<<"\n";}
+}
+namespace fast_io {
+    int read() {
+        int x = 0, f = 0; char ch = getchar();
+        while (!isdigit(ch)) f |= ch == '-', ch = getchar();
+        while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();
+        return f ? -x : x;
+    }
+    long long readLL() {
+        long long x = 0, f = 0; char ch = getchar();
+        while (!isdigit(ch)) f |= ch == '-', ch = getchar();
+        while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();
+        return f ? -x : x;
+    }
+    void read(int& x) {x = read();}
+    template<typename T> void print(T x) {
+        if (x < 0) putchar('-'), x = -x;
+        if (x >= 10) print(x / 10);
+        putchar(x % 10 + '0');
+    }
+    template<typename T> void print(T x, char let) {
+        print(x), putchar(let);
+    }
+}
+
+// using namespaces
+using namespace comfun;
+using namespace debug;
+using namespace fast_io;
+
+//------------------- start of initialize -------------------
+// initialize for all cases
+inline void init1(){
+
+}
+
+// initialize for the current subcase
+inline void init2(){
+
+}
+
+//-------------------  end of initialize  -------------------
+
+//--------------------- start of program ---------------------
+
+struct trig {
+    int a, b, c, d, e, f;
+
+    bool ok(int x, int y) {
+        if((x >= a && x <= e) || (x <= a && x >= e)) {
+            if((y >= b && y <= d) || (y <= b && y >= d)) {
+                long double z = (long double)(d - f) / (long double)(c - e) * (long double)(x - c) + d;
+                if(z == y) return 1;
+                if(z > y) {
+                    if(d < b) {
+                        return 0;
+                    } else return 1;
+                } else {
+                    if(d <= b) {
+                        return 1;
+                    } else return 0;
+                }
+            }
+        }
+        return 0;
+    }
+} ;
+vec<trig> t;
+
+inline void solve(){
+    int n = read(), q = read();
+    for(int i = 1;i<=n;++i) {
+        int a, b, c, d, e, f;
+        cin>>a>>b>>c>>d>>e>>f;
+        t.pb({a, b, c, d, e, f});
+    }
+    while(q--) {
+        int x, y;
+        cin>>x>>y;
+        int ans = 0;
+        for(auto f : t) {
+            if(f.ok(x, y)) {
+                ans++;
+            }
+        }
+        cout<<ans<<'\n';
+    }
+}
+
+
+//---------------------  end of program  ---------------------
+
+
+#define doCase 0
+#define config LOCAL
+#define kickstart 0
+#define unsync 0
+
+inline void setIO() {
+#if config
+    // configuration here
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+#if unsync
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+signed main(){
+    setIO();
+    init1();
+    srand(time(0));
+#if doCase
+    int t; t = read();
+    for(int i = 1;i<=t;i++) {
+        if(kickstart) cout<<"Case #"<< i<<": ";
+        init2();
+        solve();
+    }
+#else
+    solve();
+#endif
+    string jack = "Jack is always within you";
+    return 0;
+}
+
+
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?)
+    * do smth instead of nothing and stay organized
+    * WRITE STUFF DOWN
+    * DON'T GET STUCK ON ONE APPROACH
+*/
