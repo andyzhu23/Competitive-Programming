d95e552208763f22120746cd57d58f3e9981b0bb
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2022-01-05 11:44:06 -0800

    Update P1.cpp

--- USACO/2021/Dec/platinum/P1.cpp
+++ USACO/2021/Dec/platinum/P1.cpp
@@ -1,25 +1,6 @@
 #pragma GCC optimize(2)
 #include <bits/stdc++.h>
 using namespace std;
-
-namespace fast_io {
-    int read() {
-        int x = 0, f = 0; char ch = getchar();
-        while (!isdigit(ch)) f |= ch == '-', ch = getchar();
-        while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();
-            return f ? -x : x;
-    }
-    void read(int& x) {x = read();}
-    template<typename T> void print(T x) {
-        if (x < 0) putchar('-'), x = -x;
-        if (x >= 10) print(x / 10);
-            putchar(x % 10 + '0');
-    }
-    template<typename T> void print(T x, char let) {
-        print(x), putchar(let);
-    }
-}
-using namespace fast_io;
 #define pb push_back
 #define lc (rt << 1)
 #define rc (rt << 1 | 1)
@@ -78,14 +59,17 @@ vector<ll> dijkstra(int x) {
 }
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 #ifdef LOCAL
     freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
 #endif
-    read(n), read(k);
+    cin>>n>>k;
     build(1, 1, n);
     for(int i = 1;i<=k;++i) {
-        int c = read(), p = read(), a = read(), b = read();
+        int c, p, a, b;
+        cin>>c>>p>>a>>b;
         assign(1, 1, n, a, b, n * 4 + i, 0);
         e[n * 4 + i].pb({query(1, 1, n, c), p});
     }
@@ -100,7 +84,7 @@ int main() {
     d3 = dijkstra(0);
     for(int i = 1;i<=n;++i) {
         int x = query(1, 1, n, i);
-        print(d3[x] == INF ? -1 : d3[x], '\n');
+        cout<<(d3[x] == INF ? -1 : d3[x])<<'\n';
     }
     return 0;
 }
