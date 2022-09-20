35c0ac0f4b4ba6442b60f13c7b487f1c59979a6a
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2022-09-19 17:56:59 -0700

    Update and rename D.cpp to D1.cpp

--- codeforces/div 2/821/D1.cpp
+++ codeforces/div 2/821/D1.cpp
@@ -107,22 +107,12 @@ inline void init1(){
 
 //--------------------- start of program ---------------------
 
-const int N = 5005;
+const int N = 3005;
 char s[N], t[N];
-
-struct edge {
-    int l, r;
-    bool operator<(const edge& other) const {
-        return (r - l) < (other.r - other.l);
-    }
-    bool operator>(const edge& other) const {
-        return (r - l) > (other.r - other.l);
-    }
-};
 
 inline void solve(){
     int n = read(), x = read(), y = read();
-    vi a, cnt(n + 5), cnt2(n + 5);
+    vi a;
     set<int> vis;
     vpii bst;
     scanf("%s", s + 1);
@@ -139,58 +129,11 @@ inline void solve(){
         puts("-1");
         return;
     }
-    if(x >= y) { 
-        ll ans = y * a.size() / 2;
-        if(a.size() == 2) {
-            if(abs(a[1] - a[0]) == 1) ans = x;
-        }
-        print(ans, '\n');
-        return;
+    ll ans = y * a.size() / 2;
+    if(a.size() == 2) {
+        if(abs(a[1] - a[0]) == 1) ans = x;
     }
-    vec<edge> e;
-    for(int i = 1;i<a.size();++i) {
-        if(1ll * (a[i] - a[i - 1]) * x < y)
-            e.pb({a[i - 1], a[i]});
-    }
-    // sort(all(e), [&](edge& a, edge& b) {
-    //     return a.l < b.l;
-    // });
-    // ll ans1 = 0, ans2 = 0;
-    // for(auto[l, r] : e) {
-    //     if(cnt[l] == 0) {
-    //         ans1 += 1ll * (r - l) * x;
-    //         ++cnt[l], ++cnt[r];
-    //     } else {
-    //         ans2 += 1ll * (r - l) * x;
-    //         ++cnt2[l], ++cnt2[r];
-    //     }
-    // }
-    // int tot = 0;
-    // for(auto x : a) if(!cnt[x]) ++tot;
-    // ll ans = ans1 + 1ll * tot * y / 2;
-    // tot = 0;
-    // for(auto x : a) if(!cnt2[x]) ++tot;
-    // ckmin(ans, ans2 + 1ll * tot * y / 2);
-    // print(ans, '\n');
-    
-    ll ans = 0;
-    for(auto[l, r] : e) {
-        ans += 1ll * (r - l) * x;
-        ++cnt[l], ++cnt[r];
-        // cout<<l<<' '<<r<<'\n';
-    }
-    for(auto[l, r] : e) if(cnt[l] == 2 && cnt[r] == 2) {
-        ans -= 1ll * (r - l) * x;
-        --cnt[l], --cnt[r];
-    }
-    sort(all(e), greater<edge>());
-    for(auto[l, r] : e) if(cnt[l] == 2 || cnt[r] == 2) {
-        ans -= 1ll * (r - l) * x;
-        --cnt[l], --cnt[r];
-    }
-    int tot = 0;
-    for(auto x : a) if(!cnt[x]) ++tot;
-    print(ans + 1ll * tot * y / 2, '\n');
+    print(ans, '\n');
 }
 
 
