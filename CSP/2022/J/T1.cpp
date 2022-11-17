4ded4cd3b5f5cb7404244d7c2add7e2e507ba09e
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2022-11-16 17:34:13 -0800

    Update T1.cpp

--- CSP/2022/J/T1.cpp
+++ CSP/2022/J/T1.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
 
+int main() {
+#ifdef LOCAL
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    ll a, b;
+    cin>>a>>b;
+    if(a == 0) {
+        cout<<0<<'\n';
+        return 0;
+    }
+    ll c = 1;
+    while(b) {
+        if(b & 1) {
+            c *= a;
+            if(c > 1e9) c = 0;
+        }
+        b >>= 1;
+        a = a * a;
+        if(a > 1e9) a = 0;
+    }
+    if(c == 0) cout<<-1<<'\n';
+    else cout<<c<<'\n';
+    return 0;
+}
