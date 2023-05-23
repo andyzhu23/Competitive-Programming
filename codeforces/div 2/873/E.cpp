8d4d59e5b58420714f14e696a944802d63a0b08e
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2023-05-23 09:51:19 -0700

    Create E.cpp

---
+++ codeforces/div 2/873/E.cpp
@@ -0,0 +1,224 @@
+/*
+ * Author: Andy Zhu
+ * @date    2023-05-23 07:54:01
+ * @version 1.0.0
+ */
+
+// optimize
+#pragma GCC optimize(2)
+
+//include
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+
+// template <typename T>
+// using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// template <typename T>
+// using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+// pairs
+#define fir first
+#define sec second
+
+// segment tree
+#define lc (rt << 1)
+#define rc (rt << 1 | 1)
+
+// STL Data Structures
+#define eb emplace_back
+#define ef emplace_front
+#define pb push_back
+#define pf push_front
+#define all(v) v.begin(), v.end()
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+
+// random
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define random(a, b) rng() % (b - a + 1) + a
+
+// Data Structure Shorten
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+template <typename T> using vec = vector<T>;
+template <typename T> using us = unordered_set<T>;
+template <typename T> using os = set<T>;
+template <typename T> using ms = multiset<T>;
+template <typename T1, typename T2> using um = unordered_map<T1, T2>;
+template <typename T1, typename T2> using om = map<T1, T2>;
+template <typename T> using pq = priority_queue<T>; 
+template <typename T> using pqg = priority_queue<T, vector<T>, greater<T> >;
+using vi = vector<int>;
+using vpii = vector<pair<int, int> >;
+using vll = vector<long long>;
+using vpll = vector<pair<long long, long long> >;
+using vb = vector<bool>;
+
+// common functions
+
+namespace comfun {
+    template <typename T1, typename T2> inline void ckmax(T1& u, T2 v) { u = max(u, v); }
+    template <typename T1, typename T2> inline void ckmin(T1& u, T2 v) { u = min(u, v); }
+    template <typename T> inline T lowbit(T x){return x & (- x);}
+    template <typename T> inline T gcd(T a, T b){if(b == 0) return a; return gcd(b, a % b);}
+    template <typename T> inline T lcm(T a, T b){return a / gcd(a, b) * b;}
+    template <typename T1, typename T2> inline T1 fp(T1 a, T2 b) {T1 c = 1;while(b) {if(b & 1) c *= a;b >>= 1;a = a * a;}return c;}
+    template <typename T1, typename T2, typename T3> inline T1 fp(T1 a, T2 b, T3 mod) 
+    {T1 c = 1;while(b) {if(b & 1) c = c * a % mod;b >>= 1;a = a * a % mod;}return c;}
+    template <typename T> inline bool is_prime(T x){if(x == 1) return false; for(T i = 2; i * i <= x;i++) if(x % i == 0) return false; return true;}
+}
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+namespace fast_io {
+    int read() {int x = 0, f = 0; char ch = getchar();while (!isdigit(ch)) f |= ch == '-', ch = getchar();while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();return f ? -x : x;}
+    long long readLL() {long long x = 0, f = 0; char ch = getchar();while (!isdigit(ch)) f |= ch == '-', ch = getchar();while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();return f ? -x : x;}
+    template<typename T> T read(T& x) {x = 0; int f = 0; char ch = getchar();while (!isdigit(ch)) f |= ch == '-', ch = getchar();while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();x = f ? -x : x; return x;}
+    template<typename T> void print(T x) {if (x < 0) putchar('-'), x = -x;if (x >= 10) print(x / 10);putchar(x % 10 + '0');}
+    template<typename T> void print(T x, char let) {print(x), putchar(let);}
+}
+
+// using namespaces
+using namespace comfun;
+using namespace fast_io;
+
+// common variables
+const int inf = 0x3f3f3f3f;
+const long long llinf = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1e9 + 7;
+const int dir[8][2] = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}, {-1, 1}, {1, -1}, {-1, -1}, {1, 1}};
+const unordered_set<int> vowel = {'a', 'e', 'i', 'o', 'u'};
+
+//------------------- start of initialize --------------------
+// initialize for all cases
+inline void init1(){
+
+}
+
+//-------------------  end of initialize  --------------------
+
+//--------------------- start of program ---------------------
+
+const int N = 1e6 + 5;
+int m, n, p[N], dp[N];
+char cc[N], c[N];
+
+struct segtree {
+    int st[N << 2], lazy[N << 2];
+    void push_down(int rt) {
+        if(lazy[rt]) {
+            st[lc] = st[rc] = lazy[lc] = lazy[rc] = lazy[rt];
+            lazy[rt] = 0;
+        }
+    }
+    void build(int rt, int l, int r) {
+        st[rt] = lazy[rt] = 0;
+        if(l == r) return;
+        int mid = l + r >> 1;
+        build(lc, l, mid);
+        build(rc, mid + 1, r);
+    }
+    void update(int rt, int l, int r, int x, int y, int val) {
+        if(l == x && y == r) {
+            st[rt] = lazy[rt] = val;
+            return;
+        }
+        int mid = l + r >> 1;
+        push_down(rt);
+        if(y <= mid) update(lc, l, mid, x, y, val);
+        else if(x > mid) update(rc, mid + 1, r, x, y, val);
+        else update(lc, l, mid, x, mid, val), update(rc, mid + 1, r, mid + 1, y, val);
+    }
+    int query(int rt, int l, int r, int pos) {
+        if(l == r) return st[rt];
+        int mid = l + r >> 1;
+        push_down(rt);
+        if(pos <= mid) return query(lc, l, mid, pos);
+        else return query(rc, mid + 1, r, pos);
+    }
+} st;
+
+inline void solve() {
+    int n = 0;
+    read(m);
+    scanf("%s", cc + 1);
+    c[++n] = '#';
+    for(int i = 1;i<=m;++i) {
+        c[++n] = cc[i];
+        c[++n] = '#';
+    }
+    int r = 1, mid = 1;
+    st.build(1, 1, n);
+    for(int i = 1;i<=n;++i) {
+        p[i] = dp[i] = 0;
+        if(i < r) p[i] = min(p[mid * 2 - i], r - i);
+        while(i > p[i] + 1 && p[i] + 1 + i <= n && c[i - (p[i] + 1)] == c[i + p[i] + 1]) ++p[i];
+        if(i + p[i] > r) mid = i, r = i + p[i];
+        if(c[i] == '#') st.update(1, 1, n, i, i + p[i], i);
+    }
+    ll ans = 0;
+    for(int i = 1;i<=n;++i) if(c[i] != '#') {
+        int x = st.query(1, 1, n, i);
+        if(2 * x - i - 2 >= 0) dp[i] = dp[2 * x - i - 2] + 1;
+        ans += dp[i];
+    }
+    print(ans, '\n');
+}
+
+
+//---------------------  end of program  ---------------------
+
+
+#define doCase 1
+#define config LOCAL
+// #define kickstart
+#define unsync 0
+
+inline void setIO() {
+#if config
+    // configuration here
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+#if unsync
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+signed main(){
+    setIO();
+    init1();
+    srand(time(0));
+#if doCase
+    int t; t = read();
+    for(int i = 1;i<=t;i++) {
+#ifdef kickstart 
+        printf("Case #%d: ", i);
+#endif
+        solve();
+    }
+#else 
+    solve();
+#endif
+    string jack = "Jack is always within you";
+    return 0;
+}
+
+ 
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?)
+    * do smth instead of nothing and stay organized
+    * WRITE STUFF DOWN
+    * DON'T GET STUCK ON ONE APPROACH
+*/
