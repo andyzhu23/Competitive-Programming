94a4a35ee6acb497d678667549153b10856f1e83
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2023-09-17 14:01:44 +0800

    Create E.cpp

---
+++ codeforces/div 1/604/E.cpp
@@ -0,0 +1,265 @@
+/*
+ * Author: Andy Zhu
+ * @date    2023-09-17 13:12:05
+ * @version 1.0.0
+ */
+
+// optimize
+#pragma GCC optimize(2)
+
+//include
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+
+// template <typename T>
+// using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// template <typename T>
+// using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+// pairs
+#define fir first
+#define sec second
+
+// segment tree
+#define lc (rt << 1)
+#define rc (rt << 1 | 1)
+
+// STL Data Structures
+#define eb emplace_back
+#define ef emplace_front
+#define pb push_back
+#define pf push_front
+#define all(v) v.begin(), v.end()
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+
+// random
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define random(a, b) rng() % (b - a + 1) + a
+
+// Data Structure Shorten
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+template <typename T> using vec = vector<T>;
+template <typename T> using us = unordered_set<T>;
+template <typename T> using os = set<T>;
+template <typename T> using ms = multiset<T>;
+template <typename T1, typename T2> using um = unordered_map<T1, T2>;
+template <typename T1, typename T2> using om = map<T1, T2>;
+template <typename T> using pq = priority_queue<T>; 
+template <typename T> using pqg = priority_queue<T, vector<T>, greater<T> >;
+using vi = vector<int>;
+using vpii = vector<pair<int, int> >;
+using vll = vector<long long>;
+using vpll = vector<pair<long long, long long> >;
+using vb = vector<bool>;
+
+// common functions
+
+namespace comfun {
+    template <typename T1, typename T2> inline void ckmax(T1& u, T2 v) { u = max(u, v); }
+    template <typename T1, typename T2> inline void ckmin(T1& u, T2 v) { u = min(u, v); }
+    template <typename T> inline T lowbit(T x){return x & (- x);}
+    template <typename T> inline T gcd(T a, T b){if(b == 0) return a; return gcd(b, a % b);}
+    template <typename T> inline T lcm(T a, T b){return a / gcd(a, b) * b;}
+    template <typename T1, typename T2> inline T1 fp(T1 a, T2 b) {T1 c = 1;while(b) {if(b & 1) c *= a;b >>= 1;a = a * a;}return c;}
+    template <typename T1, typename T2, typename T3> inline T1 fp(T1 a, T2 b, T3 mod) 
+    {T1 c = 1;while(b) {if(b & 1) c = c * a % mod;b >>= 1;a = a * a % mod;}return c;}
+    template <typename T> inline bool is_prime(T x){if(x == 1) return false; for(T i = 2; i * i <= x;i++) if(x % i == 0) return false; return true;}
+}
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+namespace fast_io {
+    int read() {int x = 0, f = 0; char ch = getchar();while (!isdigit(ch)) f |= ch == '-', ch = getchar();while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();return f ? -x : x;}
+    long long readLL() {long long x = 0, f = 0; char ch = getchar();while (!isdigit(ch)) f |= ch == '-', ch = getchar();while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();return f ? -x : x;}
+    template<typename T> T read(T& x) {x = 0; int f = 0; char ch = getchar();while (!isdigit(ch)) f |= ch == '-', ch = getchar();while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();x = f ? -x : x; return x;}
+    template<typename T> void print(T x) {if (x < 0) putchar('-'), x = -x;if (x >= 10) print(x / 10);putchar(x % 10 + '0');}
+    template<typename T> void print(T x, char let) {print(x), putchar(let);}
+}
+
+// using namespaces
+using namespace comfun;
+using namespace fast_io;
+
+// common variables
+const int inf = 0x3f3f3f3f;
+const long long llinf = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1e9 + 7;
+const int dir[8][2] = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}, {-1, 1}, {1, -1}, {-1, -1}, {1, 1}};
+const unordered_set<char> vowel = {'a', 'e', 'i', 'o', 'u'};
+
+//------------------- start of initialize --------------------
+// initialize for all cases
+inline void init1(){
+
+}
+
+//-------------------  end of initialize  --------------------
+
+//--------------------- start of program ---------------------
+
+const int N = 10005;
+int n, m, mn[N], mx[N];
+
+set<pii> vis;
+int tot;
+map<pii, int> mp;
+
+struct Dinic {
+    int S, T, tot = 1, head[N], dist[N], cur[N];
+    struct edge {int u, v, w, f, nxt;} e[100005];
+    void add(int u, int v, int f, int w) {
+        e[++tot] = {u, v, w, f, head[u]};
+        head[u] = tot;
+        e[++tot] = {v, u, -w, 0, head[v]};
+        head[v] = tot;
+    }
+    bitset<N> vis;
+    bool SPFA() {
+        queue<int> q;
+        memset(dist, 0x3f, sizeof dist);
+        memcpy(cur, head, sizeof cur);
+        q.push(S);
+        dist[S] = 0;
+        while(!q.empty()) {
+            int u = q.front(); q.pop();
+            vis[u] = 0;
+            for(int i = head[u];i;i=e[i].nxt) if(e[i].f) {
+                if(dist[e[i].v] > dist[u] + e[i].w) {
+                    dist[e[i].v] = dist[u] + e[i].w;
+                    if(!vis[e[i].v]) {
+                        vis[e[i].v] = 1;
+                        q.push(e[i].v);
+                    }
+                }
+            }
+        }
+        return dist[T] != inf;
+    }
+    int cost = 0;
+    int dfs(int u, int f = inf) {
+        if(u == T) {
+            cost = f * dist[T];
+            return f;
+        }
+        vis[u] = 1;
+        int ret = 0;
+        for(int& i = cur[u];i;i=e[i].nxt) {
+            if(!vis[e[i].v] && dist[e[i].v] == dist[u] + e[i].w && e[i].f) {
+                int tmp = dfs(e[i].v, min(f, e[i].f));
+                e[i].f -= tmp;
+                e[i ^ 1].f += tmp;
+                f -= tmp;
+                ret += tmp;
+                if(f == 0) break;
+            }
+        }
+        vis[u] = 0;
+        if(ret == 0) dist[u] = 0;
+        return ret;
+    }
+    int dinic() {
+        int ans = 0;
+        while(SPFA()) ans += dfs(S);
+        return ans;
+    }
+} dinic;
+
+bool ans[55][55];
+
+inline void solve(){
+    read(n), read(m);
+    for(int i = 1;i<=n;++i) mx[i] = n - 1;
+    for(int i = 1;i<=m;++i) {
+        int u = read(), v = read();
+        ans[u][v] = 1;
+        vis.ins({u, v});
+        ++mn[u];
+        --mx[v];
+    }
+    tot = n;
+    dinic.S = 0;
+    for(int i = 1;i<=n;++i) {
+        for(int j = i + 1;j<=n;++j) if(!vis.count({i, j}) && !vis.count({j, i})) {
+            ++tot;
+            dinic.add(dinic.S, tot, 1, 0);
+            dinic.add(tot, i, 1, 0);
+            mp[(pii){i, j}] = dinic.tot - 1;
+            dinic.add(tot, j, 1, 0);
+        }
+    }
+    dinic.T = tot + 1;
+    for(int i = 1;i<=n;++i) {
+        for(int j = mn[i];j<=mx[i];++j) {
+            dinic.add(i, dinic.T, 1, 2 * j - 1);
+        }
+    }
+    dinic.dinic();
+    for(auto[u, v] : mp) {
+        if(dinic.e[v].f) ans[u.sec][u.fir] = 1;
+        else ans[u.fir][u.sec] = 1;
+    }
+    for(int i = 1;i<=n;++i) {
+        for(int j = 1;j<=n;++j) print(ans[i][j]);
+        putchar('\n');
+    }
+}
+
+
+//---------------------  end of program  ---------------------
+
+
+#define doCase 0
+#define config LOCAL
+// #define kickstart
+#define unsync 0
+
+inline void setIO() {
+#if config
+    // configuration here
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+#if unsync
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+}
+
+signed main(){
+    setIO();
+    init1();
+    srand(time(0));
+#if doCase
+    int t; t = read();
+    for(int i = 1;i<=t;i++) {
+#ifdef kickstart 
+        printf("Case #%d: ", i);
+#endif
+        solve();
+    }
+#else 
+    solve();
+#endif
+    string jack = "Jack is always within you";
+    return 0;
+}
+
+ 
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?)
+    * do smth instead of nothing and stay organized
+    * WRITE STUFF DOWN
+    * DON'T GET STUCK ON ONE APPROACH
+*/
